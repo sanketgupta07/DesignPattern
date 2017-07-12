31e0ee015170c6ffa1bed64a2d4139315c95d9ce
Sanket Gupta <sanketgupta07@gmail.com>
2017-07-13 00:30:26 +0530

    Added Observer pattern example

---
+++ DesignPattern/src/com/observer/BinaryViewObserver.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.observer;
+
+/**
+ * @author Sanket Gupta
+ *
+ */
+public class BinaryViewObserver extends Observer {
+	
+	
+
+	/**
+	 * Constructor will register {@link BinaryViewObserver} to {@link Subject}'s {@link Observer} list
+	 */
+	public BinaryViewObserver(Subject subject) {
+		this.subject = subject;
+		this.subject.addObserver(this);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.observer.Observer#update()
+	 */
+	@Override
+	public void update() {
+		System.out.println("Subject's State in Binary:"+Integer.toBinaryString(this.subject.getState()));
+
+	}
+
+}
